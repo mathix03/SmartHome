01545e805e1af6a6ef5b341bbd1b4b739780cde7
mathix03 <matheo.roulet2009@gmail.com>
2025-05-09 15:05:00 +0200

    détecteur de mouvement
    fichier avec le code du détecteur de mouvement

---
+++ mouvement.ino
@@ -0,0 +1,31 @@
+// Déclaration des pins
+const int pinCapteurMouvement = 11;
+const int pinLedJaune = 8;
+
+void setup() {
+  // Initialisation du capteur de mouvement en entrée
+  pinMode(pinCapteurMouvement, INPUT);
+  
+  // Initialisation de la LED en sortie
+  pinMode(pinLedJaune, OUTPUT);
+  
+  // Optionnel : démarrer la communication série
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Lire l'état du capteur de mouvement
+  int mouvementDetecte = digitalRead(pinCapteurMouvement);
+
+  if (mouvementDetecte == HIGH) {
+    // Allumer la LED
+    digitalWrite(pinLedJaune, HIGH);
+    Serial.println("Mouvement détecté !");
+  } else {
+    // Éteindre la LED
+    digitalWrite(pinLedJaune, LOW);
+  }
+
+  // Petite pause pour éviter les rebonds
+  delay(100);
+}
